a5d346219a51034640be43c3b7ae6e52873cfdc5
dammyjo <112008172+dammyjo@users.noreply.github.com>
2022-09-24 10:41:44 +0100

    Add files via upload

---
+++ score.py
@@ -0,0 +1,25 @@
+name=input(str("Enter your name:"))
+score=int(input("Enter your score:"))
+
+if score>=70 and score<=100:
+       print(f"{name} you got an A")
+
+elif score>= 60 and score<=69:
+     print(f"{name} you got an B")
+
+elif score>=50 and score<=59:
+     print(f"{name} you got an C")
+
+elif score>=45 and score<=49:
+     print(f"{name} you got an D")
+
+elif score>=40 and score<=44:
+     print(f"{name} you got an E")
+    
+elif score>=0 and score<=39:
+     print(f"{name} you got an F")
+
+
+
+
+\
